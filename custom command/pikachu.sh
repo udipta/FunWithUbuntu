1145321dbe466cb0e8498aa2564735f8c47c27f0
udipta <iamudi03@gmail.com>
2018-12-15 15:03:00 +0530

    my command

---
+++ custom command/pikachu.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#pypad - simple python ide
+function pypad() {
+
+	python3 /home/udipta/H\&T/PyPad/src/main.py
+}
+
+
+# labelImg - labeling the image
+function labelImg() {
+  	python3 /home/udipta/udipta/college/OpenCV_lab/labelImg/labelImg.py
+}
+
+#youtube-dl
+function playlist() {
+	youtube-dl -o "%(playlist_index)s-%(title)s.%(ext)s" -f 22 $1
+}
+
+#replaceWordinFiles
+function replace() {
+	python3 /home/udipta/myCommand/replace.py
+}
